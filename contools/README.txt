7e0bad2f1ecb6af26ed5e5ce9e12e8785424f9d2
Steve Kilbane <steve@whitecrow.demon.co.uk>
2012-05-13 15:58:36 +0100

    Added copyright statements and files for using the AGPL.

---
+++ contools/README.txt
@@ -0,0 +1,2 @@
+This is Streampunk, a Django application for developing and scheduling programmes
+for conventions and conferences. Please see LICENSE.txt for licensing conditions.
--- contools/contools/urls.py
+++ contools/contools/urls.py
@@ -1,3 +1,19 @@
+# This file is part of Streampunk, a Django application for convention programmes
+# Copyright (C) 2012 Stephen Kilbane
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+# 
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 from django.conf.urls import patterns, include, url
 from django.contrib.auth.views import login, logout, logout_then_login, password_change, password_change_done, password_reset, password_reset_done, password_reset_confirm, password_reset_complete
 from django.views.generic import DetailView
