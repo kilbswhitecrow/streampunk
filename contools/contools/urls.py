945f922f952fcde27ab1970b4d3dcdd97465a2c6
Steve Kilbane <steve@whitecrow.demon.co.uk>
2013-03-28 10:52:24 +0000

    Bumped all the copyrights for files we've changed since the year clicked over.

--- contools/contools/urls.py
+++ contools/contools/urls.py
@@ -1,5 +1,5 @@
 # This file is part of Streampunk, a Django application for convention programmes
-# Copyright (C) 2012 Stephen Kilbane
+# Copyright (C) 2012-2013 Stephen Kilbane
 # 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
