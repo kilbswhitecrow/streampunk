95e4bc209992aa8932cd81d236d5bcbb721d9ac4
Steve Kilbane <steve@whitecrow.demon.co.uk>
2013-04-11 17:38:38 +0100

    Get rid of the example test. Add some base classes to use which provide some utilities for testing common things. Populate some basic tests for an "empty" database (one with all the defaults, but no people, items or tags, and only the default rooms).
    Also add some notes on testing, and another enhancement suggestion from EightSquared.

--- contools/streampunk/tests.py
+++ contools/streampunk/tests.py
@@ -1,3 +1,17 @@
+# This file is part of Streampunk, a Django application for convention programmes
+# Copyright (C) 2013 Stephen Kilbane
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 This file demonstrates writing tests using the unittest module. These will pass
 when you run "manage.py test".
@@ -6,11 +20,156 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from django.test.client import Client
+from django.core.urlresolvers import reverse
+
+from streampunk.models import Grid
+
+class StreampunkTest(TestCase):
+
+  def contextEqual(self, key, value):
+    self.assertEqual(self.response.context[key], value)
+  def contextNotEqual(key, value):
+    self.assertNotEqual(self.response.context[key], value)
+
+  def status_okay(self):
+    self.assertEqual(self.response.status_code, 200)
+
+  def has_link_to(self, path, args=None):
+    link = '<a href="%s' % reverse(path, args=args)
+    self.assertTrue(link in self.response.content)
+
+  def no_link_to(self, path, args=None):
+    link = '<a href="%s' % reverse(path, args=args)
+    self.assertFalse(link in self.response.content)
+
+  def has_column(self, table, column):
+    t = self.response.context[table]
+    self.assertTrue(column in t.columns.names())
+
+  def no_column(self, table, column):
+    t = self.response.context[table]
+    self.assertFalse(column in t.columns.names())
+
+  def num_rows(self, table):
+    t = self.response.context[table]
+    return len(t.rows)
+
+  def find_row(self, table, rec):
+    "Locate a row which matches field/val pairs from rec, and return it"
+    t = self.response.context[table]
+    for row in t.rows:
+      if rec <= row.record:
+        # all of rec' keys appear in the row's record. Now get a list of
+        # all the keys where the values are different. If this list is
+        # empty, then this row matches the rec.
+        diffs = [ k for k in rec.keys() if rec[k] != row.record[k] ]
+        if not diffs:
+          return row.record
+    return None
+
+  def row_match(self, table, rec):
+    "Test whether table has a row which matches the field/value pairs from dict rec."
+    row = self.find_row(table, rec)
+    print "BOOGA: row_match: got %s\n" % (row)
+    return True if row else False
+
+  def has_row(self, table, rec):
+    self.assertTrue(self.row_match(table, rec))
+
+  def no_row(self, table, rec):
+    self.assertFalse(self.row_match(table, rec))
+
+class NonauthTest(StreampunkTest):
+
+  def banner(self):
+    self.has_link_to('login')
+    self.no_link_to('logout')
+    self.has_link_to('list_grids')
+    self.has_link_to('list_people')
+    self.has_link_to('list_rooms')
+    self.has_link_to('list_items')
+    self.has_link_to('list_tags')
+
+    for g in Grid.objects.all():
+      self.has_link_to('show_grid', args=(int(g.id),))
+
+class AuthTest(StreampunkTest):
+
+  def banner(self):
+    self.no_link_to('login')
+    self.has_link_to('logout')
+    self.has_link_to('list_grids')
+    self.has_link_to('list_people')
+    self.has_link_to('list_rooms')
+    self.has_link_to('list_items')
+    self.has_link_to('list_tags')
+
+class nonauth_lists(NonauthTest):
+  def setUp(self):
+    self.client = Client()
+
+  def test_main_page0(self):
+    "No items or people yet. Not logged in."
+    self.response = self.client.get(reverse('main_page'))
+    self.status_okay()
+    self.contextEqual('con_name', u'MyCon 2012')
+    self.contextEqual('num_items', 0)
+    self.contextEqual('num_people', 0)
+    self.contextEqual('num_panellists', 0)
+    self.contextEqual('budget', 0)
+    self.contextEqual('hours_scheduled', 0)
+    self.banner()
+    self.has_link_to('xml_dump')
+    self.no_link_to('new_itemperson')
+    self.no_link_to('add_tags')
+    self.no_link_to('make_con_groups')
+    self.has_column('kind_table', 'kind')
+    self.has_column('kind_table', 'count')
+
+  def test_items0(self):
+    "No items yet, but there should be a table"
+    t = 'itable'
+    self.response = self.client.get(reverse('list_items'))
+    self.status_okay()
+    self.has_column(t, 'title')
+    self.has_column(t, 'room')
+    self.has_column(t, 'start')
+    self.assertEqual(self.num_rows(t), 0)
+    self.no_link_to('new_item')
+
+  def test_people0(self):
+    "No people yet, but there should be a table"
+    t = 'ptable'
+    self.response = self.client.get(reverse('list_people'))
+    self.status_okay()
+    self.has_column(t, 'name')
+    self.has_column(t, 'badge')
+    self.no_column(t, 'memnum')
+    self.assertEqual(self.num_rows(t), 0)
+    self.no_link_to('new_person')
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+  def test_rooms0(self):
+    "We should have Nowhere and Everywhere, but only the latter is visible."
+    t = 'rtable'
+    self.response = self.client.get(reverse('list_rooms'))
+    self.status_okay()
+    self.has_column(t, 'name')
+    self.has_column(t, 'gridOrder')
+    self.has_column(t, 'description')
+    self.assertEqual(self.num_rows(t), 1)
+    self.no_row(t, { 'name': 'Nowhere' })
+    self.has_row(t, { 'name': 'Everywhere' })
+    self.no_link_to('new_room')
+
+  def test_tags0(self):
+    "No tags yet, but there should be a table"
+    t = 'ttable'
+    self.response = self.client.get(reverse('list_tags'))
+    self.status_okay()
+    self.has_column(t, 'name')
+    self.has_column(t, 'description')
+    self.assertEqual(self.num_rows(t), 0)
+    self.no_link_to('new_tag')
+
