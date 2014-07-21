3be553d4faf53abd1badb621a6b0426f100b89d6
Steve Kilbane <steve@whitecrow.demon.co.uk>
2014-07-21 18:37:20 +0100

    Determine the width of the start-time column automatically, for door listings.

--- contools/streampunk/pdf.py
+++ contools/streampunk/pdf.py
@@ -71,6 +71,20 @@ class StreampunkPdf(object):
                           borderColor=colors.red,
                           leading=self.drinks_leading))
 
+  def textWidth(self, text, style, canv=None, filename='dummy.pdf', pagesize=A4):
+    if canv==None:
+      canv = canvas.Canvas(filename, pagesize)
+    return canv.stringWidth(text, style.fontName, style.fontSize)
+
+  def maxTextWidth(self, texts, style, canv=None, filename='dummy.pdf', pagesize=A4):
+    if canv==None:
+      canv = canvas.Canvas(filename, pagesize)
+    max = 0
+    for text in texts:
+      width = self.textWidth(text, style, canv)
+      max = width if max < width else max
+    return max
+
   def begin_namecards(self):
     self.normstyle = self.styles['Normal']
     self.namestyle = self.styles['CardName']
@@ -83,14 +97,14 @@ class StreampunkPdf(object):
     self.doc.streampunk_pdf = self
 
   def end_namecards(self):
-    def namecard_template(canvas, doc):
-      canvas.saveState()
+    def namecard_template(canv, doc):
+      canv.saveState()
       # Fold
       midline = A4L[1] / 2
-      canvas.line(0, midline, A4L[0], midline)
+      canv.line(0, midline, A4L[0], midline)
       # Logo
-      canvas.drawImage(image=doc.streampunk_pdf.logo, x=20, y=20)
-      canvas.restoreState()
+      canv.drawImage(image=doc.streampunk_pdf.logo, x=20, y=20)
+      canv.restoreState()
     self.doc.build(self.story, onFirstPage=namecard_template, onLaterPages=namecard_template)
 
   def namecard(self, slot, room, title, name, is_mod):
@@ -146,24 +160,29 @@ class StreampunkPdf(object):
     self.headstyle = self.styles['DrinksHeading']
     self.doc = SimpleDocTemplate(self.response)
     self.doc.streampunk_pdf = self
+    self.cellpad = 6 # Actually, this is the default
     self.tstyle = TableStyle([
       ('GRID', (0,0), (-1, -1), 1, colors.black),
       ('VALIGN', (0,0), (-1, -1), 'TOP'),
+      ('LEFTPADDING', (0,0), (-1, -1), self.cellpad),
+      ('RIGHTPADDING', (0,0), (-1, -1), self.cellpad),
     ])
     self.story = [ ]
 
   def end_door_listings(self):
-    def drinksform_template(canvas, doc):
-      canvas.saveState()
-      canvas.drawImage(image=doc.streampunk_pdf.logo, x=20, y=20)
-      canvas.restoreState()
+    def drinksform_template(canv, doc):
+      canv.saveState()
+      canv.drawImage(image=doc.streampunk_pdf.logo, x=20, y=20)
+      canv.restoreState()
     self.doc.build(self.story, onFirstPage=drinksform_template, onLaterPages=drinksform_template)
   def door_listing(self, day, room, items):
     "Items is a list of (title, starttext) tuples."
     if len(items) == 0:
       return
+    start_width = self.maxTextWidth([ i[1] for i in items ], self.rowstyle)
+    start_width = start_width + ( 2 * self.cellpad )
     self.story.append(Paragraph("%s %s" % (room, day), self.headstyle))
     data = [ [ Paragraph(i[1], self.rowstyle), Paragraph(i[0], self.rowstyle) ] for i in items ]
-    self.story.append(Table(data, style=self.tstyle, colWidths=[ inch, None ]))
+    self.story.append(Table(data, style=self.tstyle, colWidths=[ start_width, None ]))
     self.story.append(PageBreak())
 
