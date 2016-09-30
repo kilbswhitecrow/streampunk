6ca315fd73abce6005579ec6ad8405f5428da212
Steve Kilbane <steve@whitecrow.demon.co.uk>
2016-09-30 18:21:20 +0100

    Revise the middleware to match the new style of implementation.

--- contools/streampunk/currentusermiddleware.py
+++ contools/streampunk/currentusermiddleware.py
@@ -1,14 +1,20 @@
 # This file is part of Streampunk, a Django application for convention programmes
-# This file is copied almost verbatim from this Stack Overflow post:
+# This file was originally copied almost verbatim from this Stack Overflow post:
 # http://stackoverflow.com/questions/862522/django-populate-user-id-when-saving-a-model
+# It's since been updated for revisions to the middleware model in Django.
 
 from threading import local
 
 _user = local()
 
 class CurrentUserMiddleware(object):
-  def process_request(self, request):
+  def __init__(self, get_response):
+    self.get_response = get_response
+
+  def __call__(self, request):
     _user.value = request.user
+    response = self.get_response(request)
+    return response
 
 def get_current_username():
   try:
